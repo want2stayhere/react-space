b3f6f6f4b8c75c862842527d272b81a94189032e
want2stayhere <want2stayhere@gmail.com>
2021-07-13 12:26:32 +0300

    init

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+import './style.css';
+
+import Header from './components/Header/Header';
+import Main from './components/Main/Main'
+
+function App() {
+  	return (
+		<div className="layout">
+			<Header />
+			<Main />
+			
+		</div>
+  	);
+}
+
+export default App;
